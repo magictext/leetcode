95d8770a9407bf91b68b0e2b36c0b1aeee84792c
magictext <743145068@qq.com>
2018-09-26 22:53:29 +0800

    solved with python but i can not get it

---
+++ src/allPossibleFullBinaryTrees/Solution.py
@@ -0,0 +1,10 @@
+N -= 1
+if N == 0: return [TreeNode(0)]
+ans = []
+for l in range(1, min(N, 20), 2):
+    for left in self.allPossibleFBT(l):
+        for right in self.allPossibleFBT(N - l):
+            node = TreeNode(0)
+            node.left = left
+            node.right = right
+            ans += [node]
